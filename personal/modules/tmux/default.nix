fa40cc08ec4d57201290d98f22c6f3b4ca650086
Rodrigo Gryzinski <rogryza@rogryza.com>
2020-10-14 08:39:33 -0300

    Fix tmux configs

--- personal/modules/tmux/default.nix
+++ personal/modules/tmux/default.nix
@@ -1,6 +1,17 @@
+{ pkgs, ... }:
 {
   programs.tmux = {
     enable = true;
+
+    aggressiveResize = true;
+    # vi-like keybindings
+    customPaneNavigationAndResize = true;
+    keyMode = "vi";
+    escapeTime = 0;
+    shortcut = "a";
+
     extraConfig = builtins.readFile ./tmux.conf;
+
+    plugins = [ pkgs.tmuxPlugins.sessionist ];
   };
 }
