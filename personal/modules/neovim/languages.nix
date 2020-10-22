5f87b2b8830dee9f8c7330871ca8a86cf3f8d917
Rodrigo Gryzinski <rogryza@rogryza.com>
2020-10-22 11:13:37 -0300

    Add typescript support to neovim

--- personal/modules/neovim/languages.nix
+++ personal/modules/neovim/languages.nix
@@ -5,11 +5,17 @@
       vim-javascript
       vim-jsonnet
       vim-nix
+      {
+        plugin = yats-vim;
+        config = "set re=0;";
+      }
     ];
 
     # TODO manage jdtls installation
     lsc.serverCommands.java = "jdtls";
     # TODO manage rust and cargo components installation
     lsc.serverCommands.rust = "rust-analyzer";
+
+    lsc.serverCommands.ts = "${pkgs.nodePackages_latest.typescript-language-server} --stdio";
   };
 }
