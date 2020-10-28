5163dbb2f0ce7f63d5431b2dbcec5250ffc89fc1
Rodrigo Gryzinski <rodrigo.gryzinski@truelayer.com>
2020-10-28 13:28:44 -0300

    Fix gpg key

--- personal/hosts.nix
+++ personal/hosts.nix
@@ -4,8 +4,9 @@
     home.keyboard.layout = "br";
     services.gpg-agent.sshKeys = [ "8D9342B7F994DE2B879A03F4CF270B470301BE9C" ];
   };
-  rogryza-tl = {
+  rogryza-tl = { pkgs, ... }: {
     imports = [ ./home.nix ];
+    home.packages = with pkgs; [ slack ];
     services.gpg-agent.sshKeys = [ "71014621DE5C3BFFE7C014193FE1FA377AE40438" ];
   };
 }
