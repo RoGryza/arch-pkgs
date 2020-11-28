3e8cc919f543b218d037c02fed696dc5da0b8acd
Rodrigo Gryzinski <rogryza@gmail.com>
2020-11-28 11:42:36 -0300

    Add rogryza-tl-p

--- personal/modules/gpg.nix
+++ personal/modules/gpg.nix
@@ -1,7 +1,13 @@
+{ config, ... }:
+let
+  cfg = config.services.gpg-agent;
+in
 {
   services.gpg-agent = {
     enable = true;
-    enableSshSupport = true;
+    enableSshSupport =
+      cfg.sshKeys != null &&
+      (builtins.length cfg.sshKeys) > 0;
     pinentryFlavor = "qt";
   };
 }
